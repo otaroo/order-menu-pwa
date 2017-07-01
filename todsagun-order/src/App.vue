977a3eff96605fdd6e50aade8ea7676b81ef592a
otaroo <otaroo@hotmail.co.th>
2017-07-01 08:39:52 +0700

    init

--- todsagun-order/src/App.vue
+++ todsagun-order/src/App.vue
@@ -5,6 +5,7 @@
     </header>
     <main>
       <img src="./assets/logo.png" alt="Vue.js PWA">
+      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
